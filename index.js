fbae3213ae2aa9bcd8fd4a0d00d75b75cc4fe684
Harsha643 <battuharsha6@gmail.com>
2025-01-12 15:58:01 +0530

    update data

---
+++ index.js
@@ -0,0 +1,141 @@
+
+async function fetchData() {
+    let response=await fetch("https://brick-tourmaline-case.glitch.me/products")
+    try{
+        if(!response.ok){
+            throw new Error(response.statusText)
+        }
+        let data=await response.json()
+        // console.log(data)
+        displayData(data)
+    }catch(error){
+        alert("data is fetch to failed")
+        console.error(error)
+    }
+    
+}
+fetchData()
+
+function displayData(products){
+let container =document.getElementById("container")
+
+    products.forEach((product) => {
+        // console.log(product.id)
+        let item = document.createElement("div");
+        item.innerHTML = `
+            <p>ID : ${product.id}</p>
+            <p>Name : ${product.productName}</p>
+            <p>rating:${product.rate}</p>
+            <p>price :${product.price}</p>
+            <button id = 'deleteBtn-${product.id}'>Delete</button>
+            <button id = 'editBtn-${product.id}'>Edit</button>
+            `
+            container.appendChild(item);
+        let deleteBtn = document.getElementById(`deleteBtn-${product.id}`);
+        let editBtn=document.getElementById(`editBtn-${product.id}`)
+      
+        deleteBtn.onclick = () => {
+            deleteData(product.id);
+        }
+       editBtn.onclick=()=>{
+        // v.stopPropagation()
+         editData(product.id)
+       }
+
+    })
+ 
+ 
+    
+};
+
+async function deleteData(productId) {
+// console.log(productId)
+    let response=await fetch(`https://brick-tourmaline-case.glitch.me/products/${productId}`,{"method":"DELETE"})
+    try{
+        if(!response.ok){
+            alert("deleteData function")
+            throw new Error("delete function " ,response.statusText)
+        }
+        alert("deleteData successfully")
+      
+    }catch(error){
+        alert("deletedata is failed")
+        console.error(error)
+    }
+
+    
+}
+
+async function editData(id){
+//    console.log(id)
+    let pId=document.getElementById("id")
+    let name=document.getElementById("name")
+    let image=document.getElementById("image")
+    let des=document.getElementById("des")
+    ;let rate=document.getElementById("rate")
+    let price=document.getElementById("price")
+
+    let response=await fetch(`https://brick-tourmaline-case.glitch.me/products/${id}`)
+    try{
+        if(!response.ok){
+            throw new Error(response.statusText)
+
+        }
+        let product=await response.json()
+
+        // console.log(student)
+        pId.value=product.id
+        name.value=product.productName
+        image.value=product.image
+        des.value=product.description
+        rate.value=product.rate
+        price.value=product.price
+    }catch(error){
+        alert("editData data error")
+        console.log(error)
+    }
+
+}
+
+
+async function saveData() {
+    
+    let studentId=document.getElementById("id").value
+    let productName=document.getElementById("name").value
+    let image=document.getElementById("image").value
+    let des=document.getElementById("des").value
+    let rate=document.getElementById("rate").value
+    let price=document.getElementById("price").value
+
+    let obj={
+        "productName":productName,
+        "image":image,
+        "description":des,
+        "rate":rate,
+        "price":price
+    }
+    let  method=studentId?"PUT":"POST"
+    let URL=studentId?`https://brick-tourmaline-case.glitch.me/products/${studentId}`:"https://brick-tourmaline-case.glitch.me/products"
+    let response=await fetch(URL,{
+        method,
+        "headers":{
+            "content-type":"application/json"
+           
+        },
+        "body":JSON.stringify(obj)
+    })
+    try{
+        if(!response.ok){
+            throw new Error(response.statusText)
+        }
+        alert("data updated successfully")
+     
+        fetchData()
+
+    }catch(error){
+        console.error(error)
+    }
+
+
+}
+
